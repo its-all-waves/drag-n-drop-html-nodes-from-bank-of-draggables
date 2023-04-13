ef135604bab379ed75c9a3fd15acc3f95a6a1939
Ian Painter <95984061+its-all-waves@users.noreply.github.com>
2023-04-13 16:33:09 -0500

    robustify click offset 4 win scrll n zoom

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 /* GOALS
 
-- use pointer events to implement custom drag n drop 
-- drag 1 box into another
-- must not translate upon click/tap
+- [x] use pointer events to implement custom drag n drop 
+- [x] drag 1 box into another
+- [x] must not translate upon click/tap
+- [ ] copies from origin
+- [ ] moves from receiver into another receiver
 
 */
 
@@ -13,14 +15,14 @@ const draggableDiv = document.querySelector('.draggable')
 
 draggableDiv.addEventListener('pointerdown', pointerDown)
 
+// Disallow the page from being pulled down to refresh / doing that bouncy bs
+document.documentElement.style.setProperty('overscroll-behavior', 'none')
 
 function pointerDown(e) {
 
-	// TEST DELETE
-	console.log('what the fucking fuck')
-
 	const draggable = e.target
 
+	// TODO: WHY DOESN'T THIS WORK?
 	// account for touch
 	if (e.pointerType === 'touch') {
 		draggable.style.touchAction = 'none'
@@ -36,13 +38,14 @@ function pointerDown(e) {
 	const bounds = draggable.getBoundingClientRect()
 
 	// store the offset of the click to the top/left of the draggable
-	draggable.offset = { x: e.clientX - bounds.x, y: e.clientY - bounds.y }
+	draggable.grabOffset = { x: e.clientX - bounds.x, y: e.clientY - bounds.y }
 
 	// allow the draggable to be positioned manually
 	draggable.style.position = 'absolute'
 
 	// stop translation of the draggable upon click/tap
 	updatePosition(e)
+	// snapToPointer(e)
 
 	// start listening for pointermove, pointerup
 	draggable.addEventListener('pointermove', pointerMove)
@@ -58,7 +61,7 @@ function pointerUp(e) {
 	draggable.style.position = ''
 	
 	// determine where we are so we can drop the item there, if appropriate...
-	// 'hide' the draggable from 'view' so we can at what's under it, get what's under, then 'unhide the draggable
+	// 'hide' the draggable from pointer so we can at what's under it, get what's under, then 'unhide' the draggable
 	draggable.style.pointerEvents = 'none'
 	const dropZone = document.elementFromPoint(e.clientX, e.clientY)
 	// 'unhide' the draggable
@@ -81,11 +84,24 @@ function pointerMove(e) {
 	updatePosition(e)
 }
 
+
+/* Window scroll accounts for offsets in position vals 
+due to being scrolled or zoomed (IOW, when content outside 
+the viewport). */
 function updatePosition(e) {
-	const draggable = e.target
-	const offset = draggable.offset
-	draggable.style.left = `${e.clientX - offset.x}px`
-	draggable.style.top = `${e.clientY - offset.y}px`
+	const draggable = e.target;
+	const grabOffset = draggable.grabOffset
+	const x = e.clientX - grabOffset.x + window.scrollX;
+	const y = e.clientY - grabOffset.y + window.scrollY;
+	draggable.style.left = x + 'px';
+	draggable.style.top = y + 'px';
 }
 
-
+/* ORIGINAL - Does not account for offset produced by zoom 
+and scroll (out-of-view content) */
+// function updatePosition(e) {
+// 	const draggable = e.target
+// 	const grabOffset = draggable.grabOffset
+// 	draggable.style.left = `${e.clientX - grabOffset.x}px`
+// 	draggable.style.top = `${e.clientY - grabOffset.y}px`
+// }
