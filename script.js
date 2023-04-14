33d32ca494010ddc084c30772fc787a6b417cccd
Ian Painter <95984061+its-all-waves@users.noreply.github.com>
2023-04-14 09:38:22 -0500

    now stores draggable.copy for ref, del when done IOW: stores the copy briefly in the draggable to ref it w/o creating a new var
    - now removes the no-longer-respected top and left properties of the draggable when dropped

--- script.js
+++ script.js
@@ -27,12 +27,13 @@ document.documentElement.style.setProperty('overscroll-behavior', 'none')
 function pointerDown(e) {
 	let draggable = e.target
 
+	const draggedFromOrigin = draggable.parentNode.classList.contains('origin')
 	// copy in-place if grabbed from origin, move otherwise
 	// (truly -- leave a copy behind and drag the thing you clicked)
-	if (draggable.parentNode.classList.contains('origin')) {
-		copy = copyNode(draggable)
-		copy.classList.add('copy') // DEBUG
-		// stop left-behind copy from translating under original draggable ***
+	if (draggedFromOrigin) {
+		// store the copy in the draggable to be ref'd outside of this scope *
+		const copy = draggable.copy = copyNode(draggable)
+		// stop left-behind copy from translating under original draggable **
 		copy.style.position = 'absolute'
 		draggable.parentNode.appendChild(copy)
 		// TODO: on drop, if still in origin, delete what's dragged to leave only copy
@@ -56,8 +57,10 @@ function pointerDown(e) {
 	// stop translation of the draggable upon click/tap
 	updatePosition(e)
 
-	// undoes: stop left-behind copy from translating under original draggable ***
-	copy.style.position = ''
+	// undoes: stop left-behind copy from translating under original draggable **
+	// if (draggable.copy !== null)
+	if (draggable.copy)
+		draggable.copy.style.position = ''
 
 	// start listening for pointermove, pointerup
 	draggable.addEventListener('pointermove', pointerMove)
@@ -68,25 +71,29 @@ function pointerDown(e) {
 
 /* Determine where we are so we can drop the item there, if appropriate */
 function pointerUp(e) {
-
 	const draggable = e.target
+
+	// remove all css that's no longer needed
 	draggable.classList.remove('holding')
 	draggable.style.position = ''
-	
-	// 'hide' the draggable from pointer so we can get at what's under it, 
+	draggable.style.top = ''
+	draggable.style.left = ''
+
+	// I *think* I want this to not keep unnecessary data around
+	// undoes: store the copy in the draggable to be ref'd outside of this scope *
+	delete draggable.copy
+
+	// 'hide' the draggable from pointer so we can get at what's under it,
 	// get what's under, then 'unhide' the draggable
 	draggable.style.pointerEvents = 'none'
 	const dropZone = document.elementFromPoint(e.clientX, e.clientY)
 	// 'unhide' the draggable
 	draggable.style.pointerEvents = ''
-	
+
 	// append the draggable where it's dropped if it's a valid place
-	if (
-		dropZone.classList.contains('receiver')
-		) {
-			dropZone.appendChild(draggable);
-		}
-		
+	if (dropZone.classList.contains('receiver'))
+		dropZone.appendChild(draggable)
+
 	// last step - remove all the event listeners from the draggable
 	draggable.removeEventListener('pointermove', pointerMove)
 	draggable.removeEventListener('pointerup', pointerUp)
@@ -100,12 +107,12 @@ function pointerMove(e) { updatePosition(e) }
 due to being scrolled or zoomed (IOW, when content outside 
 the viewport). */
 function updatePosition(e) {
-	const draggable = e.target;
+	const draggable = e.target
 	const grabOffset = draggable.grabOffset
-	const x = e.clientX - grabOffset.x + window.scrollX;
-	const y = e.clientY - grabOffset.y + window.scrollY;
-	draggable.style.left = x + 'px';
-	draggable.style.top = y + 'px';
+	const x = e.clientX - grabOffset.x + window.scrollX
+	const y = e.clientY - grabOffset.y + window.scrollY
+	draggable.style.left = x + 'px'
+	draggable.style.top = y + 'px'
 }
 
 function copyNode(draggedNode) {
